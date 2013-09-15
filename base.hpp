d0344dd6fdcb82597b81a39467c65e81803883a3
BOSS14420 <firefoxlinux@gmail.com>
2013-09-16 02:09:52 +0700

    fix bigint::internal::base_convert

--- base.hpp
+++ base.hpp
@@ -6,7 +6,7 @@
  *    Description:  
  *
  *        Version:  1.0
- *        Created:  09/15/2013 07:01:56 PM
+ *        Created:  09/15/201max_digit(From, To) 07:01:56 PM
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -32,47 +32,65 @@ constexpr std::size_t max_digit(Int1 base_from, Int2 base_to)
 }
 
 
-template <std::size_t sz> struct sizetype 
+template <typename IntType, IntType I> struct inttype
 {
-    static const std::size_t value = sz;
+    static const IntType value = I;
 };
+template <std::size_t sz> using sizetype = inttype<std::size_t, sz>;
 
-template <typename Int1, typename Int2, Int1 From, 
-          typename IntTrait<Int2>::DoubleInt To = IntTrait<Int2>::base,
-          typename IntTrait<Int2>::DoubleInt Quotient = To,
+
+template <typename Int1, typename Int2, Int1 From, Int2 To,
+          typename Quotient = inttype<Int2, To>,
           typename I = sizetype<0>>
 struct base_convert : 
-    public base_convert<Int1, Int2, From, To, Quotient / From,
+    public base_convert<Int1, Int2, From, To, 
+                        inttype<Int2, Quotient::value / From>,
                         sizetype<I::value+1>>
 {
     static const int dummy;
 };
 
 template <typename Int1, typename Int2, Int1 From, Int2 To>
-struct base_convert<Int1, Int2, From, To, 0, sizetype<3>>
+struct base_convert<Int1, Int2, From, To, inttype<Int2, 0>, 
+                    sizetype<max_digit(From, To)>>
 {
     static const int dummy;
-    static Int1 to[3];
+    static Int1 to[max_digit(From, To)];
 };
 
-template <typename Int1, typename Int2, Int1 From, 
-          typename IntTrait<Int2>::DoubleInt To,
-          typename IntTrait<Int2>::DoubleInt Quotient, typename I>
+template <typename Int1, typename Int2, Int1 From, Int2 To,
+          typename Quotient, typename I>
 const int base_convert<Int1, Int2, From, To, Quotient, I>::dummy
-    = base_convert<Int1, Int2, From, To, 0, sizetype<3>>
-        ::to[I::value] = Quotient % From 
-                    + 0 * base_convert<Int1, Int2, From, To, Quotient / From,
+    = base_convert<Int1, Int2, From, To, inttype<Int2, 0>, 
+                   sizetype<max_digit(From, To)>>
+        ::to[I::value] = Quotient::value % From 
+                    + 0 * base_convert<Int1, Int2, From, To, 
+                                       inttype<Int2, Quotient::value / From>,
                                        sizetype<I::value + 1>>::dummy;
 
 template <typename Int1, typename Int2, Int1 From, Int2 To>
-Int1 base_convert<Int1, Int2, From, To, 0, sizetype<3>>
-                            ::to[3];
+Int1 base_convert<Int1, Int2, From, To, inttype<Int2, 0>, 
+                  sizetype<max_digit(From, To)>>::to[max_digit(From, To)];
 
+template <typename Int1, typename Int2, Int1 From, Int2 To>
+const int base_convert<Int1, Int2, From, To, inttype<Int2, 0>, 
+                       sizetype<max_digit(From, To)>>::dummy = 0;
 
-template struct base_convert<char, std::uint8_t, 3>;
-//template struct base_convert<char, std::uint16_t, 3>;
-//template struct base_convert<char, std::uint32_t, 3>;
-//template struct base_convert<char, std::uint64_t, 3>;
+template <typename Int1, typename Int2, Int1 From>
+struct binary_base : base_convert<Int1, typename IntTrait<Int2>::DoubleInt,
+                                  From, IntTrait<Int2>::base>
+{};
+
+template struct base_convert<char, std::uint16_t, 
+                             10, IntTrait<std::uint8_t>::base>;
+template struct base_convert<char, std::uint32_t, 
+                             10, IntTrait<std::uint16_t>::base>;
+template struct base_convert<char, std::uint64_t, 
+                             10, IntTrait<std::uint32_t>::base>;
+#if defined(__GNUC__) && defined(__LP64__)
+template struct base_convert<char, __uint128_t,
+                             10, IntTrait<std::uint64_t>::base>;
+#endif
 
 }
 }
--- test-base.cc
+++ test-base.cc
@@ -22,20 +22,19 @@
 #include "base.hpp"
 
 int main(int argc, char *argv[]) {
-    typedef std::uint8_t Int;
-    typedef std::uint16_t DoubleInt;
+    typedef std::uint16_t Int;
+    typedef typename bigint::internal::IntTrait<Int>::DoubleInt DoubleInt;
 
     constexpr std::size_t bits = (sizeof (Int) << 3);
     constexpr DoubleInt max = DoubleInt(1) << bits;
     std::cout << max << " = ";
-    auto &&array = bigint::internal::base_convert<char, Int, 10>::to;
+    auto &&array = bigint::internal::binary_base<char, Int, 10>::to;
+//    auto &&array = bigint::internal::base_convert<char, DoubleInt, 10, 256>::to;
     for (auto c : array)
     {
         std::cout << static_cast<char>(c+'0') << ' ';
     }
     std::cout << '\n';
-
-    std::cout << bigint::internal::max_digit(10, max) << '\n';
 
     return 0;
 }
